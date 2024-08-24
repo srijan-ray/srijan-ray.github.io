c2c5ce417777b73d4d10776ffabe289a11465298
Srijan Ray <srijanray12@gmail.com>
2024-08-24 00:21:09 -0400

    fix: Fix build issue

--- Portfolio/astro.config.mjs
+++ Portfolio/astro.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://StrontiumComputer.github.io',
+	site: "https://StrontiumComputer.github.io",
+	base: "/Portfolio",
 });
